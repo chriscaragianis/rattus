042292824f45333cd47c99eab2b09073555d6196
Chris Caragianis <chriscaragianis@gmail.com>
2015-11-27 17:59:04 -0500

    Rattus tests pass

--- Rattus.java
+++ Rattus.java
@@ -1,12 +1,28 @@
+import java.util.Random;
+
 class Rattus {
 
   private boolean[] agit;
+  private final int agitDim = 5;
+  private Random rand;
 
   public Rattus() {
-    agit = new boolean[5];
+    agit = new boolean[agitDim];
+    rand = new Random();
   }
   public boolean[] getAgit(){
     return agit;
   }
-  public void agitate(boolean direction) {}
+  public void agitate(boolean direction) {
+    int index = rand.nextInt(agitDim);
+    int origin = index;
+    do {
+      if ((agit[index] && direction) || (!agit[index] && !direction)) {
+        index = (index + 1) % agitDim;
+      } else {
+        agit[index] = direction;
+        return;
+      }
+    } while (index != origin - 1);
+  }
 }
