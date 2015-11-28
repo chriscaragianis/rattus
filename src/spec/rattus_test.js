9e5cb932569e554d5ff088e909a7e4aaefcfc1c4
Chris Caragianis <chriscaragianis@gmail.com>
2015-11-28 13:04:30 -0500

    Site test in place

--- src/spec/rattus_test.js
+++ src/spec/rattus_test.js
@@ -5,17 +5,44 @@ const agitDim = 5;
 
 var countTrues = function(arr) {
   var count = 0;
-  for (v in arr){
-    if (v) {
+  for (v in arr) {
+    if (arr[v]) {
       count += 1;
     }
   }
   return count;
 }
 
-describe ("new rattus is empty", function () {
-  it ("should be empty on reset", function () {
+describe ("Getting the rattus", function() {
+  it ("should be empty on reset", function() {
     rattus.reset();
-    assert.equal(0, countTrues(rattus.agit));
+    assert.equal(0, countTrues(rattus.getAgit()));
+  });
+  it ("should be the right size", function() {
+    assert.equal(agitDim, rattus.getAgit().length);
   })
-})
+});
+
+describe ("Agitates properly", function() {
+  it ("should agitate empty agit down to empty agit", function() {
+    rattus.reset();
+    rattus.agitate(false);
+    assert.equal(0, countTrues(rattus.getAgit()));
+  });
+  it ("should agitate empty agit up to one true", function() {
+    rattus.agitate(true);
+    assert.equal(1, countTrues(rattus.getAgit()));
+  });
+  it ("should agitate one true down to empty", function() {
+    rattus.agitate(false);
+    assert.equal(0, countTrues(rattus.getAgit()));
+  });
+  it ("should agitate full array up to full array", function() {
+    for (v in rattus.getAgit()) {
+      rattus.agitate(true);
+    }
+    assert.equal(agitDim, countTrues(rattus.getAgit()));
+    rattus.agitate(true);
+    assert.equal(agitDim, countTrues(rattus.getAgit()));
+  })
+});
